c2b57d14242a3b42c5bb4869893c6411e4a3d728
Justin Gross <justing@jacoinc.net>
2015-08-22 16:07:40 -0700

    Testing custom layout, script, content page, and templating in Pimcore

---
+++ website/controllers/MyController.php
@@ -0,0 +1,8 @@
+<?php
+
+class MyController extends Pimcore_Controller_Action_Frontend {
+
+    public function defaultAction () {
+        $this->enableLayout();
+    }
+}
---
+++ website/views/scripts/game_content/default.php
@@ -0,0 +1,8 @@
+<?php $this->layout()->setLayout('standard'); ?>
+
+<h1><?= $this->input("headline", array("width" => 540)); ?></h1>
+
+<?php while ($this->block("contentblock")->loop()) { ?>
+    <h2><?= $this->input("subline"); ?></h2>
+    <?= $this->wysiwyg("game-content"); ?>
+<?php } ?>
