d54e68543d6e147554a9cfdc8ae1d1724d8b31c9
Justin Gross <justing@jacoinc.net>
2015-08-22 17:08:50 -0700

    test test test and more tests

--- website/views/scripts/phaser/default.php
+++ website/views/scripts/phaser/default.php
@@ -2,8 +2,6 @@
 
 <h1><?= $this->input("headline", ["width" => 540]); ?></h1>
 
-<?= $this->areablock("content"); ?>
-
 <?php while ($this->block("contentblock")->loop()) { ?>
     <h2><?= $this->input("subline"); ?></h2>
     <?= $this->wysiwyg("content"); ?>
