e665f3c1648f93e06e11063a517a3cf623cd76b5
Justin Gross <justing@jacoinc.net>
2015-08-22 18:35:17 -0700

    Started adding Codiad support

---
+++ website/controllers/CodiadController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use Website\Controller\Action;
+use Pimcore\Model\Asset;
+
+class CodiadController extends Action {
+
+    public function defaultAction () {
+        $this->enableLayout();
+    }
+}
---
+++ website/views/layouts/codiad.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<?php
+    // // we use the view helper here to have the cache buster functionality
+    // $this->headLink()->appendStylesheet('/website/static/bootstrap/css/bootstrap.css');
+    // $this->headLink()->appendStylesheet('/website/static/css/global.css');
+    // $this->headLink()->appendStylesheet('/website/static/lib/video-js/video-js.min.css', "screen");
+    // $this->headLink()->appendStylesheet('/website/static/lib/magnific/magnific.css', "screen");
+    //
+    // if($this->editmode) {
+    //     $this->headLink()->appendStylesheet('/website/static/css/editmode.css', "screen");
+    // }
+?>
+
+<?= $this->headLink(); ?>
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="/website/static/js/html5shiv.js"></script>
+<script src="/website/static/js/respond.min.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+<?= $this->layout()->content ?>
+
+<?php
+
+    // global scripts, we use the view helper here to have the cache buster functionality
+    $this->headScript()->appendFile('/website/static/js/jquery-1.11.0.min.js');
+    $this->headScript()->appendFile('/website/static/bootstrap/js/bootstrap.js');
+    $this->headScript()->appendFile('/website/static/lib/magnific/magnific.js');
+    $this->headScript()->appendFile('/website/static/lib/video-js/video.js');
+    $this->headScript()->appendFile('/website/static/js/srcset-polyfill.min.js');
+    $this->headScript()->appendFile('/website/static/phaser/js/phaser.min.js');
+
+    echo $this->headScript();
+?>
+
+<script type="text/javascript">
+
+window.onload = function() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+
+        game.load.image('logo', '/website/static/phaser/img/phaser.png');
+
+    }
+
+    function create () {
+
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+
+    }
+
+};
+
+</script>
+
+</body>
+
+</html>
